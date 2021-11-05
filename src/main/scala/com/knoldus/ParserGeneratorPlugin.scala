bc5681938e2942debc03961e402ab29c485ce704
Satyam Sangal <satyam.sangal@knoldus.com>
2021-11-05 14:30:44 +0530

    Avoid re-generating source code again if there is no change in codegen directory.

--- src/main/scala/com/knoldus/ParserGeneratorPlugin.scala
+++ src/main/scala/com/knoldus/ParserGeneratorPlugin.scala
@@ -1,7 +1,8 @@
 package com.knoldus
 
 import sbt.Keys.{javaSource, javacOptions, streams, target}
-import sbt.{AutoPlugin, Compile, Setting, settingKey, taskKey}
+import sbt.nio.Keys.fileInputs
+import sbt.{AutoPlugin, Compile, Setting, fileToFileOps, settingKey, taskKey}
 
 import scala.reflect.io.Path.jfile2path
 import scala.reflect.io.{Directory, File}
@@ -25,19 +26,29 @@ object ParserGeneratorPlugin extends AutoPlugin {
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     outputJar := File(target.value / "generatedJar" / "parser.jar"),
+    generateParserTemplate / fileInputs := Seq(codegenDirectory.value.jfile.toGlob),
     generateParserTemplate := {
-      val directory = codegenDirectory.value
-      val configFile = directory.deepFiles.find(file => file.name.equalsIgnoreCase("config.fmpp"))
-        .getOrElse(throw new Exception(s"No config.fmpp found at [$directory]"))
-      val defaultConfigFile = directory.deepFiles
-        .find(file => file.name.equalsIgnoreCase("default_config.fmpp"))
-        .getOrElse(throw new Exception(s"No default_config.fmpp found at [$directory]"))
-      val template = directory.dirs
-        .find(dir => dir.name.equalsIgnoreCase("templates"))
-        .getOrElse(throw new Exception(s"No templates found at [$directory]"))
-      new ParserGenerator(streams.value.log).generateParserTemplate(defaultConfigFile, configFile, template, outputTemplateDirectory.value)
+      if (generateParserTemplate.inputFileChanges.hasChanges || !outputTemplateDirectory.value.exists || outputTemplateDirectory.value.isEmpty) {
+        val directory = codegenDirectory.value
+        val configFile = directory.deepFiles.find(file => file.name.equalsIgnoreCase("config.fmpp"))
+          .getOrElse(throw new Exception(s"No config.fmpp found at [$directory]"))
+        val defaultConfigFile = directory.deepFiles
+          .find(file => file.name.equalsIgnoreCase("default_config.fmpp"))
+          .getOrElse(throw new Exception(s"No default_config.fmpp found at [$directory]"))
+        val template = directory.dirs
+          .find(dir => dir.name.equalsIgnoreCase("templates"))
+          .getOrElse(throw new Exception(s"No templates found at [$directory]"))
+        new ParserGenerator(streams.value.log).generateParserTemplate(defaultConfigFile, configFile, template, outputTemplateDirectory.value)
+      }
+      else outputTemplateDirectory.value.deepFiles.find(_.name.equalsIgnoreCase("parser.jj")).get
     },
-    generateParserSource := new ParserGenerator(streams.value.log).generateParserSource(generateParserTemplate.value, outputDirectory.value, packageName.value),
+    generateParserSource / fileInputs := Seq(outputTemplateDirectory.value.jfile.toGlob),
+    generateParserSource := {
+      if (generateParserSource.inputFileChanges.hasChanges || !outputDirectory.value.exists || outputDirectory.value.isEmpty) {
+        new ParserGenerator(streams.value.log).generateParserSource(generateParserTemplate.value, outputDirectory.value, packageName.value)
+      }
+      else outputDirectory.value
+    },
     compileParserSource := {
       val javaSources = Seq(
         Directory((Compile / javaSource).value),
